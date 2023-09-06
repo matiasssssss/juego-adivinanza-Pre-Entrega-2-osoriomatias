c8077dd2f4a804c03c379c4d2a2ad69f8bc81144
Matias Osorio <osoriomatias50@gmail.com>
2023-09-05 22:06:42 -0300

    first commit

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
 // Aquí el juego selecciona un número al azar
 let numeroAzar = Math.floor(Math.random() * 100) + 1;
 
-let numeroEntrada = document.getElementById('numeroEntrada');
-let mensaje = document.getElementById('mensaje');
-let intento = document.getElementById('intento');
 let intentos = 0;
+let historialIntentos = []; // Usaremos un array para mantener un registro de los intentos
 
 // Esta acción se va a ejecutar cuando toque el botón chequear
 function chequearResultado() {
     intentos++;
-    intento.textContent = intentos;
-    let numeroIngresado = parseInt(numeroEntrada.value);
+    let numeroIngresadoTexto = prompt('Introduce un número entre 1 y 100:');
+    let numeroIngresado = parseInt(numeroIngresadoTexto);
 
     if (numeroIngresado < 1 || numeroIngresado > 100 || isNaN(numeroIngresado)) {
-        mensaje.textContent = 'Por favor, introduce un número válido entre 1 y 100';
-        mensaje.style.color = 'black';
+        alert('Por favor, introduce un número válido entre 1 y 100.');
         return;
     }
 
+    historialIntentos.push(numeroIngresado); // Guardamos el intento en el historial
+
     if (numeroIngresado === numeroAzar) {
-        mensaje.textContent = '¡Felicitaciones! ¡Has adivinado el número!';
-        mensaje.style.color = 'green';
-        numeroEntrada.disabled = true;
+        alert(`¡Felicitaciones! ¡Has adivinado el número en ${intentos} intentos!\nHistorial de intentos: ${historialIntentos.join(', ')}`);
+        reiniciarJuego();
     } else if (numeroIngresado < numeroAzar) {
-        mensaje.textContent = '¡Más alto! El número es mayor al que dijiste';
-        mensaje.style.color = 'red';
+        alert('¡Más alto! El número es mayor al que dijiste');
     } else {
-        mensaje.textContent = '¡Más bajo! El número es menor al que dijiste';
-        mensaje.style.color = 'red';
+        alert('¡Más bajo! El número es menor al que dijiste');
     }
+}
+
+// Función para reiniciar el juego
+function reiniciarJuego() {
+    numeroAzar = Math.floor(Math.random() * 100) + 1;
+    intentos = 0;
+    historialIntentos = [];
 }
